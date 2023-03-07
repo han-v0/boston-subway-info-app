5ba883f14eb0168856a37c324bf33582cb9cc565
Cindy Vo <cindyhvo824@gmail.com>
2023-03-07 16:20:27 -0500

    added types

--- boston-subway-info-app-frontend/src/components/DisplayTable/DisplayTable.tsx
+++ boston-subway-info-app-frontend/src/components/DisplayTable/DisplayTable.tsx
@@ -5,25 +5,30 @@ import Button from "react-bootstrap/Button";
 import { getRoutes, getStops } from "../../api";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Route, StopsInfo, Stop } from "../../utils/types";
 
 const DisplayTable = () => {
-  const [stops, setStops] = React.useState<any>([]);
-  const [routes, setRoutes] = React.useState<any>([]);
+  const [stops, setStops] = React.useState<StopsInfo>({
+    route_id: "",
+    stops: [],
+  });
+  const [routes, setRoutes] = React.useState<Array<Route>>([]);
 
-  const handleOnClick = (route_id: any) => {
+  const handleOnClick = (route_id: string) => {
     getStops(route_id)
-      .then((data: any) => {
-        setStops(data);
+      .then((data: Array<Stop>) => {
+        setStops({ route_id, stops: data });
       })
       .catch();
   };
+
   const handleClear = () => {
-    setStops([]);
+    setStops({ route_id: "", stops: [] });
   };
 
   React.useEffect(() => {
     getRoutes()
-      .then((data: any) => {
+      .then((data: Array<Route>) => {
         setRoutes(data);
       })
       .catch();
@@ -31,72 +36,85 @@ const DisplayTable = () => {
   React.useEffect(() => {}, [stops]);
   return (
     <Row>
-      <Col>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Route Id #</th>
-              <th>Route Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {routes.map((data: any) => {
-              return (
-                <tr>
-                  <td>
-                    <p
-                      style={{
-                        cursor: "pointer",
-                        color: "#0d6efd",
-                        textDecoration: "underline",
-                      }}
-                      onClick={() => {
-                        handleOnClick(data.route_id);
-                      }}
-                    >
-                      {data.route_id}
-                    </p>
-                  </td>
-                  <td>{data.name}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
+      <Col style={{ paddingLeft: "20px" }}>
+        <Row style={{ display: "flex", justifyContent: "center" }}>
+          All Subway Routes in Boston
+        </Row>
+        <Row>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Route ID</th>
+                <th>Route Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {routes.map((route: Route) => {
+                return (
+                  <tr key={route.route_id}>
+                    <td>
+                      <p
+                        style={{
+                          cursor: "pointer",
+                          color: "#0d6efd",
+                          textDecoration: "underline",
+                        }}
+                        onClick={() => {
+                          handleOnClick(route.route_id);
+                        }}
+                      >
+                        {route.route_id}
+                      </p>
+                    </td>
+                    <td>{route.name}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Row>
       </Col>
 
-      <Col>
+      <Col style={{ paddingLeft: "50px" }}>
         <div style={{ paddingBottom: "10px" }}>
           <Button variant="outline-dark" onClick={handleClear}>
             Clear
           </Button>
         </div>
 
-        {stops.length > 0 ? (
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Stop Id #</th>
-                <th>Stop Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              {stops.map((data: any) => {
-                return (
+        {stops.stops.length > 0 ? (
+          <>
+            <Row
+              style={{ display: "flex", justifyContent: "center" }}
+            >{`Stops for Route ${stops.route_id}`}</Row>
+            <Row>
+              <Table striped bordered hover>
+                <thead>
                   <tr>
-                    <td>{data.stop_id}</td>
-                    <td>{data.name}</td>
+                    <th>Stop ID</th>
+                    <th>Stop Name</th>
                   </tr>
-                );
-              })}
-            </tbody>
-          </Table>
+                </thead>
+                <tbody>
+                  {stops.stops.map((stop: Stop) => {
+                    return (
+                      <tr key={stop.stop_id}>
+                        <td>{stop.stop_id}</td>
+                        <td>{stop.name}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </Row>
+          </>
         ) : (
           <Card border="light" style={{ width: "18rem" }}>
             <Card.Body>
               <Card.Text>
                 Click on the id on the left of the route to look at for all
-                stops on that route.
+                stops on that route. Use the Clear button below to clear the
+                Stops table.
               </Card.Text>
             </Card.Body>
           </Card>
