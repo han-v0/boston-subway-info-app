d91b25969b8f67b109f847faf2beea1e5eb3a14d
Cindy Vo <cindyhvo824@gmail.com>
2023-03-12 15:53:59 -0400

    added pagination to api

--- boston-subway-info-app-backend/api/adapters.py
+++ boston-subway-info-app-backend/api/adapters.py
@@ -1,17 +1,17 @@
+from typing import List
 from fastapi import HTTPException, Path
 
 from api.models import Route, Stop
-from api.schemas import FastStops, FastRoutes
 
 
-def retrieve_all_routes():
-    routes = Route.objects.all()
+def retrieve_all_routes() -> List[Route]:
+    routes = list(Route.objects.all())
     return routes
 
 
-def retrieve_stops_of_a_route(route_id: str = Path(..., description="get stops of a route from db")):
+def retrieve_stops_of_a_route(route_id: str = Path(..., description="get stops of a route from db")) -> List[Stop]:
     route = Route.objects.filter(route_id=route_id)
-    print(route[0].stops.all())
     if not route or not route[0].stops:
         raise HTTPException(status_code=404, detail=f"Error in retrieving stops for {route_id}")
-    return route[0].stops.all()
+    stops_of_a_route = list(route[0].stops.all())
+    return stops_of_a_route
